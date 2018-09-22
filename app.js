6b6a1eaa028831b6dcae590daf4fb435fb5764c4
juliely8619 <july8619@yahoo.com>
2018-09-22 16:22:33 -0700

    started making objects for store

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 'use strict';
 
+//random function taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random (which was given by Nicholas)
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var firstAndPikeStore {
+  minCust: 23,
+  maxCust: 65,
+  aveCookie: 6.3,
+  randCustPerHr: getRandomInt();
+
+}
+
+var seatacAirportStore {
+  minCust: 3,
+  maxCust: 24,
+  aveCookie: 1.2
+  randCustPerHr: getRandomInt();
+}
+
+var seattleCenterStore {
+  minCust: 11,
+  maxCust: 38,
+  aveCookie: 3.7
+  randCustPerHr: getRandomInt();
+
+}
+
+var capitolHillStore {
+  minCust: 20,
+  maxCust: 38,
+  aveCookie: 2.3
+  randCustPerHr: getRandomInt();
+
+}
+
+var alikStore {
+  minCust: 2,
+  maxCust: 16,
+  aveCookie: 4.6
+  randCustPerHr: getRandomInt();
+}
 
