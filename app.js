3dc8e59060a202f5cc7c0d63a79d24fe2fd4b937
juliely8619 <july8619@yahoo.com>
2018-09-22 17:17:10 -0700

    have the opbejcts, pausing to re-read chap 5 about DOMs

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 'use strict';
 
 //random function taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random (which was given by Nicholas)
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+/*
+==================
+notes and hints:
+-will need to use DOM for getting it to show lists on sales page
+==================
+*/
+
+//array of cookies purch
+var storesCookiesPurchArray = [];
 
 var firstAndPikeStore {
   minCust: 23,
   maxCust: 65,
   aveCookie: 6.3,
-  randCustPerHr: getRandomInt();
+  randCustPerHr: getRandomInt(this.minCust, this.maxCust);
 
 }
+//calc cookies per hour
+//how to plan for that after 12 it resets to 1
+for (var i = 6; i< 21)
+  var firstAndPikeCookiesPurch = firstAndPikeStore.aveCookie * firstAndPikeStore.randCustPerHr)
+
 
 var seatacAirportStore {
   minCust: 3,
   maxCust: 24,
   aveCookie: 1.2
-  randCustPerHr: getRandomInt();
+  randCustPerHr: getRandomInt(this.minCust, this.maxCust);
 }
 
 var seattleCenterStore {
   minCust: 11,
   maxCust: 38,
   aveCookie: 3.7
-  randCustPerHr: getRandomInt();
+  randCustPerHr: getRandomInt(this.minCust, this.maxCust);
 
 }
 
@@ -32,7 +49,7 @@ var capitolHillStore {
   minCust: 20,
   maxCust: 38,
   aveCookie: 2.3
-  randCustPerHr: getRandomInt();
+  randCustPerHr: getRandomInt(this.minCust, this.maxCust);
 
 }
 
@@ -40,6 +57,6 @@ var alikStore {
   minCust: 2,
   maxCust: 16,
   aveCookie: 4.6
-  randCustPerHr: getRandomInt();
+  randCustPerHr: getRandomInt(this.minCust, this.maxCust);
 }
 
