a0e258e29b51161bbdc7db7fa99cf248b4384179
juliely8619 <july8619@yahoo.com>
2018-09-26 00:20:29 -0700

    have constructore functions, and printing to website, but quitting for the night

--- app.js
+++ app.js
@@ -10,15 +10,57 @@ function Store(name, minCust, maxCust, aveCookies) {
   this.minCustPerHr = minCust;
   this.maxCustPerHour = maxCust;
   this.aveCookiesPerCustPerSale = aveCookies;
+  this.cookiesSoldEachHr = [];
 }
 
 //adding methods for object constructor function
+//random function we built in class
+//calcs random customer per hour AND mults it against ave cookies so we  get random cookie sales per hr
+Store.prototype.CalcCookiesSalePerHr = function () {
+  var randAmt = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHr + 1) + this.minCustPerHr);
+  return Math.round(randAmt * this.aveCookiesPerCustPerSale);
+}
+//builds an array of the cookies sold each hour
+Store.prototype.buildCookiesSoldEachHrArray = function() {
+  for (var k = 0; k < 15; k++) {
+    this.cookiesSoldEachHr.push(this.CalcCookiesSalePerHr());
+  }
 
+}
 
+//rendering back to site
+Store.prototype.rendersHours = function() {
+  //ref a section in the HTML
+  this.CalcCookiesSalePerHr();
+  var storesContainer = document.getElementById('stores');
+  var headerElement = document.createElement('h2');
+  headerElement.textContent= this.name;
+  storesContainer.appendChild(headerElement);
+
+  var ulEl= document.createElement('ul');
+
+  for (var y in this.cookiesSoldEachHr) { 
+    //this is the same as
+    // for (var = y; y < this.cookiesSoldEachHours.length; y++)
+    var listItemEl = document.createElement('li');
+    listItemEl.textContent = this.cookiesSoldEachHr[y];
+    ulEl.appendChild(listItemEl);
+
+  }
+  storesContainer.appendChild(ulEl);
+}
 
 //creating objects
-var pikeAndFirstStore = Store('First and Pike', 23, 65, 6.3);
-var seaTacStore = Store('Sea Tac', 3, 24, 1.2);
-var seattleCenterStore = Store('Seattle Center', 11, 38, 3.7);
-var capitolHillStore = Store('Capitol Hill', 20, 38, 2.3);
-var alkiStore = Store('Alki', 2, 16, 4.6);
+var pikeAndFirstStore = new Store('First and Pike', 23, 65, 6.3);
+var seaTacStore = new Store('Sea Tac', 3, 24, 1.2);
+var seattleCenterStore = new Store('Seattle Center', 11, 38, 3.7);
+var capitolHillStore = new Store('Capitol Hill', 20, 38, 2.3);
+var alkiStore = new Store('Alki', 2, 16, 4.6);
+
+var storeArray = [pikeAndFirstStore, seaTacStore, seattleCenterStore, capitolHillStore,alkiStore];
+
+for (var i = 0; i < storeArray.length; i++) {
+  storeArray[i].CalcCookiesSalePerHr();
+  storeArray[i].buildCookiesSoldEachHrArray();
+  storeArray[i].rendersHours();
+}
